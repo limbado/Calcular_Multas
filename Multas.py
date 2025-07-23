27dbfeb7d19a11bca4678404b8fac239e79eb4c0
limbado <limbado23@gmail.com>
2025-07-23 12:36:15 +0100

    multa_fora_localidade
    adicionada a função multa_fora.localidade e respetivas velocidades/multas

--- Multas.py
+++ Multas.py
@@ -7,3 +7,11 @@ def multa_localidade(v):
         return 120
     else:
         return 320
+    
+def multa_fora_localidade(v):
+    if v <= 90:
+        return 0
+    elif v < 120:
+        return 60
+    else:
+        return 120
